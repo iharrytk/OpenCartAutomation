95fd9f37a4566442f0664cb7b5a5db86f00696b9
haritha thiagu kumar <haritha.vf130@gmail.com>
2024-03-10 17:14:42 -0500

    Login Page page actions and testcases created

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -1,5 +1,59 @@
 package com.qa.opencart.pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
+
+	private WebDriver driver;
+
+	// Constructor of the class
+	public LoginPage(WebDriver driver) {
+
+		this.driver = driver;
+	}
+
+	// By locators of the class
+	By username = By.id("input-email");
+	By password = By.id("input-password");
+	By footerlist = By.xpath("//footer//li/a");
+	By loginbtn = By.xpath("//input[@type='submit']");
+	By forgotPassword = By.linkText("Forgotten Password");
+
+	// public page actions/methods
+	public String getTitleLoginPage() {
+		String title = driver.getTitle();
+		System.out.println("the title of the page is:" + title);
+		return title;
+
+	}
+
+	public String getCurrentURLofLoginPage() {
+		String url = driver.getCurrentUrl();
+		System.out.println("the url of the page is:" + url);
+		return url;
+
+	}
+
+	public List<String> getFooterList() {
+
+		List<WebElement> footerList = driver.findElements(footerlist);
+		List<String> values = new ArrayList<String>();
+		for (WebElement webElement : footerList) {
+			String footerlistvalue = webElement.getText();
+			values.add(footerlistvalue);
+		}
+		return values;
+
+	}
+
+	public boolean isFPLinkDisplayed() {
+		return driver.findElement(forgotPassword).isDisplayed();
+
+	}
 
 }
--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -1,5 +1,66 @@
 package com.qa.opencart.tests;
 
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import com.qa.opencart.pages.LoginPage;
+
 public class LoginPageTest {
+
+	protected LoginPage lp;
+	WebDriver driver;
+
+	@BeforeTest
+	public void setUp() {
+
+		driver = new ChromeDriver();
+		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		lp = new LoginPage(driver);
+
+	}
+	//Validate if the Login Page Title is displayed as "Account Login"
+	@Test
+	public void loginPageTitleTest() {
+		String title = lp.getTitleLoginPage();
+		Assert.assertEquals(title, "Account Login");
+
+	}
+	//Validate if the URL of Login Page is displayed as per the requirement and has the word login
+	@Test
+	public void loginPageurlTest() {
+		String url = lp.getCurrentURLofLoginPage();
+		Assert.assertTrue(url.contains("route=account/login"));
+
+	}
+	//Validate if the Login Page footer section has 15 hyperlinks.
+	@Test
+	public void footersListTest() {
+		List<String> footervalues = lp.getFooterList();
+		int footerListCount = footervalues.size();
+		Assert.assertEquals(footerListCount, 15);
+
+	}
+	
+	//Validate if the Login Page has the forgot password link.
+	@Test
+	public void forgotPasswordLinkTest() {
+		Boolean linkpresence = lp.isFPLinkDisplayed();
+		Assert.assertTrue(linkpresence);
+
+	}
+
+	@AfterTest
+	public void tearDown() {
+
+		driver.quit();
+
+	}
 
 }
