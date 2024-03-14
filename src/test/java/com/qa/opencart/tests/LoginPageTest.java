e0c3c71a880591246771352a0f6742d377ddfed7
haritha thiagu kumar <haritha.vf130@gmail.com>
2024-03-13 22:40:40 -0500

    added AccountsPage testcases and basic SearchPage class

--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -3,14 +3,10 @@ package com.qa.opencart.tests;
 import java.util.List;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.qa.opencart.base.BaseTest;
-import com.qa.opencart.pages.LoginPage;
 
 public class LoginPageTest extends BaseTest {
 
