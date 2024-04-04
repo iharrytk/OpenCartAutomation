c4ddee48eb3db82f3eb92960dc5636ee633bbb05
haritha thiagu kumar <haritha.vf130@gmail.com>
2024-04-04 12:05:32 -0500

    registration page has been tested using data providers from the ExcelUtil

--- src/main/java/com/qa/opencart/utils/AppConstants.java
+++ src/main/java/com/qa/opencart/utils/AppConstants.java
@@ -1,15 +1,19 @@
 package com.qa.opencart.utils;
 
 public class AppConstants {
-	
-	
-	public static final String LOGIN_PAGE_TITLE_VALUE="Account Login";
-	public static final String ACCOUNTS_PAGE_TITLE_VALUE="My Account" ;
-	public static final String LOGIN_PAGE_URL_FRACTION_VALUE="route=account/login";
-	public static final String ACCOUNTS_PAGE_URL_FRACTION_VALUE="route=account/account";
-	
-	public static final int SHORT_DEFAULT_WAIT=5;
-	public static final int MEDIUM_DEFAULT_WAIT=10;
-	public static final int LONG_DEFAULT_WAIT=15;
+
+	public static final String LOGIN_PAGE_TITLE_VALUE = "Account Login";
+	public static final String ACCOUNTS_PAGE_TITLE_VALUE = "My Account";
+	public static final String LOGIN_PAGE_URL_FRACTION_VALUE = "route=account/login";
+	public static final String ACCOUNTS_PAGE_URL_FRACTION_VALUE = "route=account/account";
+
+	public static final int SHORT_DEFAULT_WAIT = 5;
+	public static final int MEDIUM_DEFAULT_WAIT = 10;
+	public static final int LONG_DEFAULT_WAIT = 15;
 	public static final String INCORRECT_LOGIN_ERROR_MESSAGE = "Warning: No match for E-Mail Address and/or Password.";
+
+	public static final String USER_RESG_SUCCESS_MESSG = "Your Account Has Been Created!";
+
+	// ******************Sheet Names*****************//
+	public static final String REGISTER_SHEET_NAME = "register";
 }
--- src/test/java/com/qa/opencart/tests/RegistrationPageTest.java
+++ src/test/java/com/qa/opencart/tests/RegistrationPageTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.qa.opencart.base.BaseTest;
+import com.qa.opencart.utils.AppConstants;
+import com.qa.opencart.utils.ExcelUtil;
 
 public class RegistrationPageTest extends BaseTest {
 
@@ -15,28 +17,46 @@ public class RegistrationPageTest extends BaseTest {
 		rp = lp.goToRegisterPage();
 
 	}
-	
+
 	@DataProvider
 	public Object[][] userRegistrationData() {
-		
-		return new Object[][] {
-			{"alex","xander","4587857452","alex","No"},
-			{"coco","channel","4587857452","coco","Yes"},
-			{"dax","daxxy","4585289632","daxxy","No"}
-		};
-	}
-	
-	public String randomEmail() {
-		
-		return "testautomation"+System.currentTimeMillis()+"@gmail.com";
-	}
-	//Validate whether we the user is able to perform registration in the Register Page
-	@Test(dataProvider="userRegistrationData")
-	public void doregisterTest(String firtname,String lastname,String phonenumber,String password,String subscribe) {
 
-		crp = rp.doRegisterUser(firtname, lastname,randomEmail(), phonenumber, phonenumber,subscribe);
+		return new Object[][] { { "alex", "xander", "4587857452", "alex", "No" },
+				{ "coco", "channel", "4587857452", "coco", "Yes" }, { "dax", "daxxy", "4585289632", "daxxy", "No" } };
+	}
+
+	public String randomEmail() {
+
+		return "testautomation" + System.currentTimeMillis() + "@gmail.com";
+	}
+
+	// Validate whether we the user is able to perform registration in the Register
+	// Page
+	@Test(dataProvider = "userRegistrationData")
+	public void doregisterTest(String firtname, String lastname, String phonenumber, String password,
+			String subscribe) {
+
+		crp = rp.doRegisterUser(firtname, lastname, randomEmail(), phonenumber, phonenumber, subscribe);
 		String registrationMessage = crp.getRegistrationMesg();
 		Assert.assertEquals(registrationMessage, "Your Account Has Been Created!");
+	}
+
+	@DataProvider(name = "regExcelData")
+	public Object[][] getRegExcelTestData() {
+
+		Object regData[][] = ExcelUtil.getTestData(AppConstants.REGISTER_SHEET_NAME);
+		return regData;
+	}
+
+	// Validate whether we the user is able to perform registration in the Register
+	// Page
+	@Test(dataProvider = "regExcelData")
+	public void doregisterwithExcelDataTest(String firtname, String lastname, String phonenumber, String password,
+			String subscribe) {
+
+		crp = rp.doRegisterUser(firtname, lastname, randomEmail(), phonenumber, phonenumber, subscribe);
+		String registrationMessage = crp.getRegistrationMesg();
+		Assert.assertEquals(registrationMessage, AppConstants.USER_RESG_SUCCESS_MESSG);
 	}
 
 }
